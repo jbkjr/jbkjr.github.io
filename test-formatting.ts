1895909764131b4f996b2dd3011e7b1ec2d4d66b
Jack Koch <jack@jbkjr.me>
2025-10-22 18:04:58 -0400

    Remove test file for linting setup

--- test-formatting.ts
+++
@@ -1,6 +0,0 @@
-// Test file with intentionally bad formatting
-const badFormatting = "too many spaces"
-const missing_semicolon = "this will pass because we don't use semicolons"
-const goodFormatting = "this is fine"
-
-export { badFormatting, goodFormatting }
